06bb18a1b2d90e5efbd65c2093d486843b5c3740
iikeda-cq <i.ikeda@caraquri.com>
2021-03-24 16:17:45 +0900

    ViewBindingの導入

--- app/src/main/java/com/google/codelab/hotpepperapiapp/MainActivity.kt
+++ app/src/main/java/com/google/codelab/hotpepperapiapp/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.google.codelab.hotpepperapiapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.codelab.hotpepperapiapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val mOnNavigationItemSelectedListener =
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+
+            when (item.itemId) {
+                R.id.navigation_list -> {
+
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_map -> {
+
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_favorite -> {
+
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        setContentView(binding.root)
     }
 }
