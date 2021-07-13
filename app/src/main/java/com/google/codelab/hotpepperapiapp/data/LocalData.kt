f587399938581d933bc6804a7efac252599ea696
iikeda-cq <i.ikeda@caraquri.com>
2021-07-13 21:15:10 +0900

    Realmの修正

--- app/src/main/java/com/google/codelab/hotpepperapiapp/data/LocalData.kt
+++ app/src/main/java/com/google/codelab/hotpepperapiapp/data/LocalData.kt
@@ -2,18 +2,19 @@ package com.google.codelab.hotpepperapiapp.data
 
 import com.google.codelab.hotpepperapiapp.ext.addStore
 import com.google.codelab.hotpepperapiapp.ext.deleteStore
-import com.google.codelab.hotpepperapiapp.ext.hasFavoriteStore
 import com.google.codelab.hotpepperapiapp.ext.fetchStores
+import com.google.codelab.hotpepperapiapp.ext.hasFavoriteStore
 import com.google.codelab.hotpepperapiapp.model.Store
 import io.reactivex.rxjava3.core.Completable
 import io.realm.Realm
 import io.realm.RealmResults
 import javax.inject.Inject
 
-class LocalData @Inject constructor(private val realm: Realm){
+class LocalData @Inject constructor(private val realm: Realm) {
     fun fetchLocalStoreIds(): RealmResults<Store> {
-        // Caused by: java.lang.IllegalStateException: This Realm instance has already been closed, making it unusable.
-        return realm.fetchStores()
+        return Realm.getDefaultInstance().use { realm ->
+            realm.fetchStores()
+        }
     }
 
     fun addFavoriteStore(storeId: String): Completable {
